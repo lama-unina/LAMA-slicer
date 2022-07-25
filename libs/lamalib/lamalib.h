9ee285e961982eaad0b62c7815be5b2f065a934e
salvatorebramante <salvatore.bramante@yahoo.it>
2022-07-25 16:52:02 +0200

    Added a lamalib header to include the entire core

---
+++ libs/lamalib/lamalib.h
@@ -0,0 +1,22 @@
+//
+// Created by Salvatore Bramante on 25/07/22.
+//
+
+#ifndef LAMALIB_H
+#define LAMALIB_H
+
+#include <thread>
+#include <vector>
+#include <queue>
+
+namespace Lamalib{
+    // costant values that can be useful
+    constexpr auto LAMALIB_VERSION = "0.0.1";
+    constexpr auto EPSILON = 1e-4;
+    constexpr auto PI = 3.141592653589793238;
+    constexpr auto SCALE = 0.000001;
+
+}
+using namespace Lamalib;
+
+#endif
